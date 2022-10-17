e2607609e2185fa3e35f3cb65e92df572014d6b1
filippova_t <tanya-fef@yandex.ru>
2022-10-17 22:48:09 +0800

    комит new2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("hello 3");
+        System.out.println("hello world 3");
     }
 }
